06d0721e1a3257cbe17d42997d0a5556bb184df0
chubinashvili <chubinashvili2003@gmail.com>
2022-12-02 20:12:09 +0400

    server setup

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from "express";
+const app = express();
+import dotenv from 'dotenv';
+dotenv.config();
+
+import connectDB from './db/connect.js';
+
+// middleware
+import notFoundMiddleware from './middleware/not-found.js';
+import errorHandlerMiddleware from './middleware/error-handler.js';
+
+app.get('/', (req, res) => {
+    res.send('Welcome');
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL);
+        app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+    } catch (err) {
+        console.log(error);
+    }
+}
+
+start();
