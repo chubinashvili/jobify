85627d93b6d3daf8463cc697b9f543c02d1dfd06
chubinashvili <chubinashvili2003@gmail.com>
2022-12-02 23:14:29 +0400

    created User Model

---
+++ models/User.js
@@ -0,0 +1,36 @@
+import mongoose, { mongo } from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: 'String',
+        required: [true, 'Please provide name'],
+        minlength: 3,
+        maxlength: 20,
+        trim: true,
+    },
+    email: {
+        type: 'String',
+        required: [true, 'Please provide email'],
+        unique: true,
+    },
+    password: {
+        type: 'String',
+        required: [true, 'Please provide password'],
+        minlength: 6,
+    },
+    lastName: {
+        type: 'String',
+        maxlength: 20,
+        trim: true,
+        default: 'lastName',
+    },
+    location: {
+        type: 'String',
+        maxlength: 20,
+        trim: true,
+        default: 'my city',
+    },
+
+})  
+
+export default mongoose.model('User', UserSchema);
