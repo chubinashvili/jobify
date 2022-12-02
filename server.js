6469775fb5ddb420f51d703dc6665926e5c342c7
chubinashvili <chubinashvili2003@gmail.com>
2022-12-02 20:36:35 +0400

    added auth structure

--- server.js
+++ server.js
@@ -3,15 +3,23 @@ const app = express();
 import dotenv from 'dotenv';
 dotenv.config();
 
+// db and authenticateUser
 import connectDB from './db/connect.js';
+
+// routers
+import authRouter from './routes/authRoutes.js';
 
 // middleware
 import notFoundMiddleware from './middleware/not-found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Welcome');
 });
+
+app.use('/api/v1/auth', authRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
