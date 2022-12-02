b6984b0571a3669f7d05d85d1ddd9616ba4fb94b
chubinashvili <chubinashvili2003@gmail.com>
2022-12-03 00:15:16 +0400

    added email validator

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
-import mongoose, { mongo } from 'mongoose';
+import mongoose from 'mongoose';
+import validator from 'validator';
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -12,6 +13,10 @@ const UserSchema = new mongoose.Schema({
         type: 'String',
         required: [true, 'Please provide email'],
         unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: 'Please provide a valid email',
+        },
     },
     password: {
         type: 'String',
