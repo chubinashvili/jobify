ff8f76902d320be71907d402dbe135a612a5b46b
chubinashvili <chubinashvili2003@gmail.com>
2022-12-02 21:00:50 +0400

    added jobs structure

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import connectDB from './db/connect.js';
 
 // routers
 import authRouter from './routes/authRoutes.js';
+import jobsRouter from './routes/jobsRoutes.js';
 
 // middleware
 import notFoundMiddleware from './middleware/not-found.js';
@@ -20,6 +21,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/auth', jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
