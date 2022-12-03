ebec7a933632b941c82e30d719f267bbc39e7f11
chubinashvili <chubinashvili2003@gmail.com>
2022-12-04 03:53:15 +0400

    added createJWT function, removed password from response and completed register back-end

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,7 +13,14 @@ const register = async (req, res) => {
         throw new BadRequestError('Email already in use');
     }
     const user = await User.create({ name, email, password });
-    res.status(StatusCodes.CREATED).json({ user });
+    const token = user.createJWT();
+    res.status(StatusCodes.CREATED)
+        .json({ user: { 
+            email: user.email,
+            lastName: user.lastName,
+            location: user.location,
+            name: user.name
+        }, token, location: user.location });
 }
 const login = async (req, res) => {
     res.send('login user');
