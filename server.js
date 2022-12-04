cc7f92632a850cbd7cd9ab748b8284889b24e4bd
chubinashvili <chubinashvili2003@gmail.com>
2022-12-04 17:58:35 +0400

    connected front-end and server

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ import errorHandlerMiddleware from './middleware/error-handler.js';
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/api/v1', (req, res) => {
     res.send('Welcome');
 });
 
