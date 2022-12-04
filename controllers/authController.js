f23dd19ae482a0710e895052ceab9b0db1d7084e
chubinashvili <chubinashvili2003@gmail.com>
2022-12-04 21:32:53 +0400

    added login controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 import User from '../models/User.js';
 import { StatusCodes } from 'http-status-codes';
-import { BadRequestError } from '../errors/index.js';
+import { BadRequestError, UnauthenticatedError } from '../errors/index.js';
 
 const register = async (req, res) => {
     const { name, email, password } = req.body;
@@ -23,7 +23,21 @@ const register = async (req, res) => {
         }, token, location: user.location });
 }
 const login = async (req, res) => {
-    res.send('login user');
+    const { email, password } = req.body;
+    if(!email || !password) {
+        throw new BadRequestError('Please provide all values');
+    }
+    const user = await User.findOne({ email }).select('+password');
+    if(!user) {
+        throw new UnauthenticatedError('Invalid Credentials');
+    }
+    const isPasswordCorrect = await user.comparePassword(password);
+    if(!isPasswordCorrect) {
+        throw new UnauthenticatedError('Invalid Credentials');
+    }
+    const token = user.createJWT();
+    user.password = undefined;
+    res.status(StatusCodes.OK).json({ user, token, location: user.location})
 }
 const updateUser = async (req, res) => {
     res.send('updateUser');
