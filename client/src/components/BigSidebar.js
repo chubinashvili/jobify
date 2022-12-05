1fa991ab38ef5f6d0cd121699ee091c15880d67d
chubinashvili <chubinashvili2003@gmail.com>
2022-12-05 11:00:12 +0400

    added big sidebar

--- client/src/components/BigSidebar.js
+++ client/src/components/BigSidebar.js
@@ -1,9 +1,20 @@
+import { useAppContext } from '../context/appContext';
+import NavLinks from './NavLinks';
+import Logo from './Logo';
 import Wrapper from '../assets/wrappers/BigSidebar';
 
 const BigSidebar = () => {
+  const { showSidebar } = useAppContext();
   return (
     <Wrapper>
-      <h4>big sidebar</h4>
+      <div className={showSidebar ? 'sidebar-container' : 'sidebar-container show-sidebar'}>
+        <div className='content'>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
     </Wrapper>
   )
 }
