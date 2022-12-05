50b39d1ff163eaa0f15714693394592f6b2f90ea
chubinashvili <chubinashvili2003@gmail.com>
2022-12-05 10:25:04 +0400

    added small sidebar

--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -1,9 +1,44 @@
-import Wrapper from '../assets/wrappers/SmallSidebar';
+import Wrapper from '../assets/wrappers/SmallSidebar.js';
+import { FaTimes } from 'react-icons/fa';
+import { useAppContext } from '../context/appContext.js';
+import { NavLink } from 'react-router-dom';
+import links from '../utils/links.js';
+import Logo from './Logo.js';
 
 const SmallSidebar = () => {
+  const { showSidebar, toggleSidebar } = useAppContext();
   return (
     <Wrapper>
-      <h4>small sidebar</h4>
+      <div className={showSidebar ? 'sidebar-container show-sidebar' : 'sidebar-container'}>
+        <div className='content'>
+          <button 
+            type='button' 
+            className='close-btn' 
+            onClick={() => toggleSidebar(!showSidebar)}
+          >
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className='nav-links'>
+            {links.map(link => {
+              const { text, path, id, icon } = link;
+              return (
+                <NavLink
+                  to={path}
+                  className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}
+                  key={id}
+                  onClick={toggleSidebar}
+                >
+                  <span className='icon'>{icon}</span>
+                  {text}
+                </NavLink>
+              ) 
+            })}
+          </div>
+        </div>
+      </div>
     </Wrapper>
   )
 }
