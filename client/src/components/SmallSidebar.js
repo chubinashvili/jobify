2112d3c4fad498937fc3f0537e63076ae5f2a39f
chubinashvili <chubinashvili2003@gmail.com>
2022-12-05 10:48:37 +0400

    added NavLinks component

--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -1,9 +1,8 @@
 import Wrapper from '../assets/wrappers/SmallSidebar.js';
 import { FaTimes } from 'react-icons/fa';
 import { useAppContext } from '../context/appContext.js';
-import { NavLink } from 'react-router-dom';
-import links from '../utils/links.js';
 import Logo from './Logo.js';
+import NavLinks from './NavLinks.js';
 
 const SmallSidebar = () => {
   const { showSidebar, toggleSidebar } = useAppContext();
@@ -21,22 +20,7 @@ const SmallSidebar = () => {
           <header>
             <Logo />
           </header>
-          <div className='nav-links'>
-            {links.map(link => {
-              const { text, path, id, icon } = link;
-              return (
-                <NavLink
-                  to={path}
-                  className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}
-                  key={id}
-                  onClick={toggleSidebar}
-                >
-                  <span className='icon'>{icon}</span>
-                  {text}
-                </NavLink>
-              ) 
-            })}
-          </div>
+          <NavLinks toggleSidebar={toggleSidebar} />
         </div>
       </div>
     </Wrapper>
