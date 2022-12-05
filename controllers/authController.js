fe1e67427358488ed0bbbe4bd3aa958100a4273d
chubinashvili <chubinashvili2003@gmail.com>
2022-12-05 20:19:22 +0400

    added updateUser controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -40,8 +40,26 @@ const login = async (req, res) => {
     res.status(StatusCodes.OK).json({ user, token, location: user.location})
 }
 const updateUser = async (req, res) => {
-    console.log(req.user)
-    res.send('updateUser');
+    const { email, name, lastName, location } = req.body;
+    if (!email || !name || !lastName || !location) {
+        throw new BadRequestError('Please provide all values');
+    }
+    
+    const user = await User.findOne({ _id: req.user.userId });
+
+    user.email = email;
+    user.name = name;
+    user.lastName = lastName;
+    user.location = location;
+
+    await user.save();
+
+    const token = user.createJWT();
+    res.status(StatusCodes.OK).json({
+        user,
+        token, 
+        location: user.location, 
+    });
 }
 
 export { register, login, updateUser };
