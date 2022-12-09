6394067f22db63e35402f5eeb4c62735556fd6fe
chubinashvili <chubinashvili2003@gmail.com>
2022-12-09 17:13:22 +0400

    added monthly applications server side

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -3,6 +3,7 @@ import { StatusCodes } from 'http-status-codes';
 import { BadRequestError, NotFoundError } from '../errors/index.js';
 import checkPermissions from '../utils/checkPermissions.js';
 import mongoose from 'mongoose';
+import moment from 'moment';
 
 const createJob = async (req, res) => {
     const { position, company } = req.body;
@@ -64,7 +65,7 @@ const showStats = async (req, res) => {
         { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId )} },
         { $group: { _id: '$status', count: { $sum: 1 }} },
     ]);
-
+    
     stats = stats.reduce((acc, current) => {
         const { _id: title, count } = current;
         acc[title] = count;
@@ -76,7 +77,29 @@ const showStats = async (req, res) => {
         interview: stats.interview || 0,
         declined: stats.declined || 0,
     };
-    let monthlyApplications = [];
+    let monthlyApplications = await Job.aggregate([
+        { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId )} },
+        { $group: {
+            _id: {
+                year: { $year: '$createdAt' },
+                month: { $month: '$createdAt' },
+            },
+            count: { $sum: 1 },
+          }
+        },
+        { $sort: { '_id.year': -1, '_id.month': -1 } },
+        { $limit: 6 },
+    ]);
+
+    monthlyApplications = monthlyApplications
+        .map((item) => {
+            const { _id: { year, month }, count } = item;
+            const date = moment()
+                .month(month - 1)
+                .year(year)
+                .format('MMM Y');
+            return { date, count };
+        }).reverse();
 
     res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 } 
