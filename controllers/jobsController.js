9ba3da72b78bad133aed37dbe7fe4bd4cc200024
chubinashvili <chubinashvili2003@gmail.com>
2022-12-10 03:36:07 +0400

    added pagination server side

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -54,12 +54,20 @@ const getAllJobs = async (req, res) => {
     if (sort === 'z-a') {
         result = result.sort('-position');
     }
-    
+
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+    result = result.skip(skip).limit(limit);
+     
     const jobs = await result;
+
+    const totalJobs = await Job.countDocuments(queryObject);
+    const numOfPages = Math.ceil(totalJobs / limit );
 
     res
        .status(StatusCodes.OK)
-       .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
+       .json({ jobs, totalJobs, numOfPages });
 } 
 
 const updateJob = async (req, res) => {
