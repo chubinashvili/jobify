f885511dfcc8efbc4055745eb27b21583f190250
chubinashvili <chubinashvili2003@gmail.com>
2022-12-10 18:07:40 +0400

    added security packages

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ dotenv.config();
 import 'express-async-errors';
 import morgan from 'morgan';
 
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+import helmet from 'helmet';
+import xss from 'xss-clean';
+import mongoSanitize from 'express-mongo-sanitize';
+
 // db and authenticateUser
 import connectDB from './db/connect.js';
 
@@ -20,18 +28,24 @@ import authenticateUser from './middleware/auth.js';
 if(process.env.NODE_ENV !== 'production') {
     app.use(morgan('dev'));
 }
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// only when ready to deploy
+app.use(express.static(path.resolve(__dirname, './client/build')));
+
 app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.json({ msg: 'Welcome!'});
-});
-
-app.get('/api/v1', (req, res) => {
-    res.json({ msg: 'API'});
-});
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
+
+// only when ready to deploy
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+});
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
