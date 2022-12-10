d7bb0f5dcc58d48c2a2f700009509b87c03c1cde
chubinashvili <chubinashvili2003@gmail.com>
2022-12-10 06:04:07 +0400

    fixed warnings

--- client/src/components/JobsContainer.js
+++ client/src/components/JobsContainer.js
@@ -21,6 +21,7 @@ const JobsContainer = () => {
 
     useEffect(() => {
         getJobs();
+        // eslint-disable-next-line
     }, [page, search, searchStatus, searchType, sort]);
 
     if (isLoading) {
--- client/src/pages/dashboard/SharedLayout.js
+++ client/src/pages/dashboard/SharedLayout.js
@@ -1,4 +1,4 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { Navbar, SmallSidebar, BigSidebar } from '../../components';
 import Wrapper from '../../assets/wrappers/SharedLayout.js';
 
